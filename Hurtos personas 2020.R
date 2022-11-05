a5eb1261e752a56098d77cb1161434aa5360ea5c
daniell419 <111817055+daniell419@users.noreply.github.com>
2022-11-04 23:50:58 -0500

    Resultados

---
+++ Hurtos personas 2020.R
@@ -0,0 +1,44 @@
+### grafica de hurtos en bogota 
+
+#Los datos se pueden descargar de: https://datosabiertos.bogota.gov.co/dataset/delito-de-alto-impacto-bogota-d-c
+
+### cambiar por el directorio que descargaste los datos.
+crimen_bogota <- readOGR(dsn = "D:/noveno semestre/big data/problem set 3/dataPS3", layer = "DAIUPZ", GDAL1_integer64_policy = TRUE)
+crimen_bogota <- spTransform(crimen_bogota, CRS("+proj=longlat +datum=WGS84 +no_defs"))
+crimen_bogota = st_as_sf(crimen_bogota)
+
+## quita algunas localidades que no son de interes
+crimen_bogota = subset(crimen_bogota, select = c(CMHP20CONT, CMIUUPLA, CMNOMUPLA)) %>% 
+  filter(grepl("Rio",CMNOMUPLA)==FALSE)
+
+
+## una paleta de colores azul.
+pal <- colorNumeric(
+  palette = "Blues",
+  domain = crimen_bogota$CMHP20CONT)
+
+## anadir los labels
+labels <- sprintf(
+  "<strong>%s</strong><br/>%g hurtos a persona",
+  crimen_bogota$CMNOMUPLA, crimen_bogota$CMHP20CONT
+) %>% lapply(htmltools::HTML)
+
+
+leaflet() %>% addTiles() %>% addPolygons(data=crimen_bogota,
+                                         fillColor = ~pal(CMHP20CONT),
+                                         weight = 2,
+                                         opacity = 1,
+                                         color = "white",
+                                         dashArray = "2",
+                                         fillOpacity = 1,
+                                         highlightOptions = highlightOptions(
+                                           weight = 5,
+                                           color = "#666",
+                                           dashArray = "",
+                                           fillOpacity = 0.7,
+                                           bringToFront = TRUE),
+                                         label = labels,
+                                         labelOptions = labelOptions(
+                                           style = list("font-weight" = "normal", padding = "3px 8px"),
+                                           textsize = "15px",
+                                           direction = "auto"))
